d32c435eb82801b1a90ad7f7391ed8f690310d41
prezi-o <oli.prezi@gmail.com>
2024-04-22 16:25:12 +0200

    Nuget csomagok importálása

---
+++ UnitTestProject.Test/SzuresTeszt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+//using Drivers_KliensAlkalamazas.Controllers;
+using System.Activities;
+
+namespace UnitTestProject.Test
+{
+    public class SzuresTeszt
+    {
+        [Test]
+        public void SKUValidator(string sku)
+        {
+
+        }
+    }
+}
